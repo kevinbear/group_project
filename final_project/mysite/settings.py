9e1b5f23794d5e332055811b3c5a6b88131837a2
YinBo Kuo <kuokeyahoocomtw@gmail.com>
2025-03-30 23:24:03 -0700

    add allowed_hosts into host

--- final_project/mysite/settings.py
+++ final_project/mysite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-!1*yp@e$*%8r=r4(hcdr5^4hiz5%#d5^jcgt0go_!q7-=s(e_1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "group-project-z6xq.onrender.com",
+]
 
 
 # Application definition
